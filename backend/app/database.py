d0fa3790de4876b6a972e9c4cf5a5de34c41f44e
vinaykatikireddy <vinaykatikireddy@hotmail.com>
2026-09-06 15:16:27 +0530

    removed unknown option

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,12 +8,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 
-print(f"Using SQLite database at {DATABASE_URL}")
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
