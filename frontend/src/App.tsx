6fbf5f56d6d3ba04d1012214a1554565e0e10a49
vinaykatikireddy <vinaykatikireddy@hotmail.com>
2026-09-06 16:55:44 +0530

    fixed app.tsx routing problem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout /> }>
+        <Route path="/ai-web-vuln-sim" element={<Layout /> }>
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
